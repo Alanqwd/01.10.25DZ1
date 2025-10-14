b0480f59bab7188a31a27e159bdec81a3ee2a610
User <User@WIN-7RSVVMUHAJG>
2025-10-14 20:38:11 +0500

    Добавьте файлы проекта.

---
+++ 01.10.25DZ1/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _01._10._25DZ1;
+
+namespace _1._10._25Dz
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<ToDo> CasesList;
+
+        public MainWindow()
+        {
+            CasesList = new List<ToDo>();
+
+
+
+
+
+            InitializeComponent();
+
+            CasesList.Add(new ToDo("Приготовить покушать", new DateTime(2024, 1, 15), "Нет описания"));
+            CasesList.Add(new ToDo("Поработать", new DateTime(2024, 1, 20), "Съездить на совещание в Москву"));
+            CasesList.Add(new ToDo("Отдохнуть", new DateTime(2024, 2, 1), "Съездить в отпуск в Сочи"));
+            List.ItemsSource = CasesList;
+
+
+        }
+        private void Button_CLick(object sender, RoutedEventArgs e)
+        {
+
+            Case cas = new Case();
+            cas.Show();
+        }
+
+    }
+}
---
+++ 01.10.25DZ1/ToDo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._10._25Dz
+{
+    public class ToDo
+    {
+        public string CaseName { get; set; }
+        public DateTime? DateExecution { get; set; }
+        public string DescriptionCase { get; set; }
+
+
+        public ToDo(string casename, DateTime? dateexecution, string descriptioncase)
+        {
+            CaseName = casename;
+            DateExecution = dateexecution;
+            DescriptionCase = descriptioncase;
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
